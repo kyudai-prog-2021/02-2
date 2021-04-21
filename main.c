316b031c9e14288a286746224523bad0f44e82a1
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2021-04-21 14:29:43 +0900

    main関数調整

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int main(){
-    int a;
-    scanf("%d",&a);
-    printf("%d\n",a*2);
-    printf("%d\n",a*3);
-    return 0;
+main(void)
+{
+    int i,j;
+    unsigned int u;
+    float f;
+    
+    i = 077;
+    j = 3.14;
+    u = -255;
+    f = 10
+    
+    printf("i = %d\n", i);
+    printf("j = %d\n", j);
+    printf("u = %u\n,"u);
+    printf("f = %d\n", f);
 }
--- test1.sh
+++ test1.sh
@@ -2,42 +2,14 @@
 rm ./test1 
 gcc main.c -o test1
 
-function func(){
-input=$(<data1/input.txt)
-./test1 << EOF
-$input
-EOF
-}
-
-result=$(func)
-result=(${result// /\n})
+./test1 >./data1/result.txt
 
 git log --oneline -n 1 | cut -d ' ' -f 1  >>log.txt
-output=(`cat data1/output.txt|xargs`)
 
-num_ans=${#output[*]}
-check=0
+./test1 > ./data1/result.txt
+check=`diff ./data1/output.txt ./data1/result.txt`
 
-#出力の数が一致しているかの検証
-if test ${#result[*]} -ne $num_ans
-then
-    echo 'test1 failed1'
-    exit 1
-fi
-
-#出力値の比較
-for ((i=0;i < $num_ans;i++));
-do
-    if test ${result[i]} -ne ${output[i]};
-    then
-        echo 'test1 failed'
-        exit 1
-    else
-        check=`expr $check + 1`
-    fi
-done
-
-if test $check -eq $M
+if test $check -z
 then
     echo "test1 success"
     exit 0
